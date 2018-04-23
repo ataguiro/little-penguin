0b9386593853d89f414c9f537827018127dd5dac
root <root@ataguiro.ataguiro.org>
2018-04-23 16:36:00 +0200

    update

--- 07/.id.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/root/little-pinguin/07/id.ko := ld -r -m elf_x86_64 -z max-page-size=0x200000 -T ./scripts/module-common.lds --build-id  -o /root/little-pinguin/07/id.ko /root/little-pinguin/07/id.o /root/little-pinguin/07/id.mod.o ;  true
