dd06f7060ff2ddf8eaea52bdcb3589e407c3cb81
root <root@ataguiro.ataguiro.org>
2018-04-27 00:05:51 +0200

    update

--- 08/main.c
+++ 08/main.c
@@ -11,114 +11,64 @@ MODULE_LICENSE("LICENSE");
 MODULE_AUTHOR("Louis Solofrizzo <louis@ne02ptzero.me>");
 MODULE_DESCRIPTION("Useless module");
 
-static ssize_t
-myfd_read
-(
- struct
- file *fp,
- char
- __user *user,
- size_t
- size,
- loff_t
- *offs);
-static ssize_t
-myfd_write(
-		struct
-		file *fp,
-		const char
-		__user *user,
-		size_t
-		size,
-		loff_t
-		*offs);
-static struct
-file_operations   myfd_fops = {
-	.owner
-		= THIS_MODULE, .read    = &myfd_read, .write
-		= &myfd_write
+static ssize_t myfd_read (struct file *fp, char __user *user, size_t size, loff_t *offs);
+static ssize_t myfd_write(struct file *fp, const char __user *user, size_t size, loff_t *offs);
+
+static struct file_operations myfd_fops = {
+	.owner = THIS_MODULE,
+	.read = &myfd_read,
+	.write = &myfd_write
 };
-static struct
-miscdevice
-myfd_device = {
-	.minor
-		= MISC_DYNAMIC_MINOR,.name
-		= "reverse",
-	.fops
-		= &myfd_fops };
-char
-str[PAGE_SIZE];
-char
-*tmp;
-static int
-__init myfd_init
-(
- void
- ) {
-	int
-		retval;
-	retval = misc_register(&(*(&(myfd_device))));
-	return
-		1;
-}
-static void
-__exit myfd_cleanup
-(
- void
- ) {
-}
-ssize_t
-myfd_read
-(
- struct
- file *fp,
- char
- __user *user,
- size_t
- size,
- loff_t
- *offs)
+
+static struct miscdevice myfd_device = {
+	.minor = MISC_DYNAMIC_MINOR,
+	.name = "reverse",
+	.fops = &myfd_fops
+};
+
+char str[PAGE_SIZE];
+char *tmp;
+
+static int __init myfd_init(void)
 {
-	size_t
-		t, i;
-	char
-		*tmp2;
+	int retval;
+
+	retval = misc_register(&myfd_device);
+	return retval;
+}
+
+static void __exit myfd_cleanup(void)
+{
+	misc_deregister(&myfd_device);
+	return ;
+}
+
+ssize_t myfd_read(struct file *fp, char __user *user, size_t size, loff_t *offs)
+{
+	size_t t, i;
+	char *tmp2;
+
 	/***************
 	 * Malloc like a boss
 	 ***************/
-	tmp2 = kmalloc(
-			sizeof
-			(
-			 char
-			) * PAGE_SIZE * 2, GFP_KERNEL);
+
+	tmp2 = kmalloc(sizeof(char) * PAGE_SIZE * 2, GFP_KERNEL);
 	tmp = tmp2;
-	for
-		(t = strlen(str) - 1, i = 0; t >= 0; t--, i++) {
-			tmp[i] = str[t];
-		}
+	for (t = strlen(str) - 1, i = 0; t >= 0; t--, i++) {
+		tmp[i] = str[t];
+	}
 	tmp[i] = 0x0;
-	return
-		simple_read_from_buffer(user, size, offs, tmp, i);
+	return simple_read_from_buffer(user, size, offs, tmp, i);
 }
-ssize_t
-myfd_write
-(
- struct
- file *fp,
- const char
- __user *user,
- size_t
- size,
- loff_t
- *offs) {
-	ssize_t
-		res;
-	res = 0;
+
+ssize_t myfd_write(struct file *fp, const char __user *user, size_t size, loff_t *offs)
+{
+	ssize_t res;
+
 	res = simple_write_to_buffer(str, size, offs, user, size) + 1;
-	// 0x0 = ’\0’
 	str[size + 1] = 0x0;
-	return
-		res;
+	return res;
 }
+
 module_init(myfd_init);
 module_exit(myfd_cleanup);
