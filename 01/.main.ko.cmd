8e0d0fdd8890db3ec1daaadb0b163f4392bbdd71
root <root@ataguiro.ataguiro.org>
2018-04-27 14:46:15 +0200

    ok

---
+++ 01/.main.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/root/little-pinguin/01/main.ko := ld -r -m elf_x86_64 -z max-page-size=0x200000 -T ./scripts/module-common.lds --build-id  -o /root/little-pinguin/01/main.ko /root/little-pinguin/01/main.o /root/little-pinguin/01/main.mod.o ;  true
---
+++ 01/.tmp_versions/main.mod
@@ -0,0 +1,3 @@
+/root/little-pinguin/01/main.ko
+/root/little-pinguin/01/main.o
+
